e251cddcb03e6bcaad3745cd65e87aaf3a7166dc
MoriCiao <bboynoice@gmail.com>
2025-06-07 14:33:28 +0800

    圖片連結修正嘗試...5

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const BgImg = () => {
     >
       <img
         className=" w-full object-cover opacity-[.8] brightness-50"
-        src="./public/pictrue/Bg-img.jpg"
+        src="/pictrue/Bg-img.jpg"
         alt="BgImg"
       />
     </motion.div>
