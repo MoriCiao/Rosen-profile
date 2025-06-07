36e66bbb292af1d388a0d2cbbae8069bcdb4eec1
MoriCiao <bboynoice@gmail.com>
2025-06-07 15:42:39 +0800

    base修正

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Rosen-profile/",
   plugins: [react()],
   base: "/Rosen-profile/", // ← ⬅⬅⬅ 一定要 base 是根目錄
 });
